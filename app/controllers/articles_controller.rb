467f5077a66474a91a51d4391f30f8b7b44abd68
gtsofa <jlsnyule@yahoo.com>
2017-04-04 23:37:28 +0300

    added flash to the update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,7 @@ def update
   @article = Article.find(params[:id])
   @article.update(article_params)
 
+  flash.notice = "Article '#{@article.title}' has been successful updated!"
   redirect_to article_path(@article)
 end
 
