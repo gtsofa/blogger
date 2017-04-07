83c5ff3090029e2638f13283029aff0575220b93
gtsofa <jlsnyule@yahoo.com>
2017-04-07 20:28:39 +0300

    re-arrange methods on articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,22 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+  @article = Article.find(params[:id])
+
+  end
   def create
     @article = Article.new(article_params)
     @article.save
     flash.notice = "Article '#{@article.title}' has been successful created!"
   redirect_to article_path(@article)
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' has been successful updated!"
+    redirect_to article_path(@article)
   end
   def destroy
     @article = Article.find(params[:id])
@@ -25,16 +36,5 @@ class ArticlesController < ApplicationController
     flash.notice = "Article '#{@article.title}' has been successful deleted!"
     redirect_to article_path
   end
-  def edit
-  @article = Article.find(params[:id])
-
-end
-def update
-  @article = Article.find(params[:id])
-  @article.update(article_params)
-
-  flash.notice = "Article '#{@article.title}' has been successful updated!"
-  redirect_to article_path(@article)
-end
 
 end
