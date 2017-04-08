3ca47fc467f2498e1f0262fa7f20a3ce863dac44
gtsofa <jlsnyule@yahoo.com>
2017-04-08 16:41:11 +0300

    creating the logging in feature

---
+++ app/models/author.rb
@@ -0,0 +1,9 @@
+=begin
+class Author < ApplicationRecord
+  authenticates_with_sorcery!
+end
+=end
+class Author < ActiveRecord::Base
+  authenticates_with_sorcery!
+  validates_confirmation_of :password, message: "should match confirmation", if: :password
+end
